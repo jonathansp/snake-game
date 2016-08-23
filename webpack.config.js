540742c5c547a4f3cadfb7d57250ac5c0510df34
Jonathan Prates <jonathan.prates@corp.globo.com>
2016-08-23 14:46:58 -0300

    chore: fixing json format

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,15 @@
 module.exports = {
-  entry: ['babel-polyfill', "./src/main.js"],
+    entry: ['babel-polyfill', "./src/main.js"],
 
-  output: {
-    path: __dirname,
-    filename: "./dist/snake-game.js"
-  },
+    output: {
+        path: __dirname,
+        filename: "./dist/snake-game.js"
+    },
 
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader', // 'babel-loader' is also a legal name to reference
-      }
-    ]
-  }
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+        }]
+    }
 };
